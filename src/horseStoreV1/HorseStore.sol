c2198b550450718e3adbb5c41ad5c9ffac35cb09
Mikhail Antonov <mikhailantonov@Mikhails-MacBook-Air-2.local>
2024-03-11 23:19:58 +0400

    added HorseStorage contract written in Yul

--- src/horseStoreV1/HorseStore.sol
+++ src/horseStoreV1/HorseStore.sol
@@ -5,8 +5,8 @@ pragma solidity ^0.8.20;
 contract HorseStore {
     uint256 numberOfHorses;
 
-    function updateHorseNumber(uint256 newNumberOfHorces) external {
-        numberOfHorses = newNumberOfHorces;
+    function updateHorseNumber(uint256 newNumberOfHorses) external {
+        numberOfHorses = newNumberOfHorses;
     }
 
     function readNumberOfHorses() external view returns (uint256) {
